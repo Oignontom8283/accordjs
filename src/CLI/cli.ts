992aea0d9586f4961003a236f8e6a422222a69d8
﻿ <98918089+Oignontom8283@users.noreply.github.com>
2025-07-07 17:36:13 +0200

    Fixed console messages for development mode and unknown commands

--- src/CLI/cli.ts
+++ src/CLI/cli.ts
@@ -5,7 +5,7 @@ import { ACCORDJS_DEVLOPMENT_MODE } from "../constant";
 import init from "./init";
 import start from './start';
 
-ACCORDJS_DEVLOPMENT_MODE && console.log("⚠️ Development mode is enabled. This is not recommended for production use.".yellow.italic);
+ACCORDJS_DEVLOPMENT_MODE && console.log("🚧 Development mode is enabled. This is not recommended for production use.".yellow.italic);
 
 const args = process.argv.slice(2);
 const command = args[0];
@@ -18,7 +18,7 @@ const command = args[0];
         start()
     }
     else {
-        console.log(`\n❌ Unknown command: ${command}. Please use \`accordjs help\` for a list of available commands.\n`.red);
+        console.log(`\n❌ Unknown command: \`${command}\`. Please use \`accordjs help\` for a list of available commands.\n`.red);
         process.exit(1);
     }
 })();
