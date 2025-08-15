c225fd777d46998662f38799e8702d6713147b1f
Oignontom8283 <tfarin83@gmail.com>
2025-08-15 12:46:23 +0200

    Refactor dev function to remove initializeFRomFileSystem and enhance module loading logic

--- src/CLI/dev.ts
+++ src/CLI/dev.ts
@@ -1,10 +1,22 @@
 import path from "path";
-import { initializeFRomFileSystem } from "..";
 import { configDotenv } from "dotenv";
+import { getAllFilesRecursive, loadModuleFromFile } from "..";
+import { SOURCE_CODE_FILE_NAME_REGEX, SOURCE_CODE_FOLDER_NAME_REGEX } from "../constant";
+import { AnyCreateReturn } from "../types";
 
 export default async function dev() {
     configDotenv({
         "path": path.join(process.cwd(), ".env")
     })
-    initializeFRomFileSystem(path.join(process.cwd(), "src"))
-}
+
+    const workDir = path.join(process.cwd(), "src");
+
+    const filesPaths = getAllFilesRecursive(
+        workDir,
+        SOURCE_CODE_FILE_NAME_REGEX, // Match source code files
+        SOURCE_CODE_FOLDER_NAME_REGEX // Match source code folders
+    )
+
+    const rawModules:{module:any, path:string}[] = filesPaths.map(filePath => ({ module: loadModuleFromFile(filePath), path: filePath }));
+
+    // const modules: {export: AnyCreateReturn, id: string} = 
