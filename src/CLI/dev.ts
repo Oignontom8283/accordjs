7ceb13c58746406f16f42149e4f23c3db7cb4cd1
﻿ <98918089+Oignontom8283@users.noreply.github.com>
2025-08-17 02:31:38 +0200

    Refactor dev function to include ensureFramworkModule import and improve module loading logic

--- src/CLI/dev.ts
+++ src/CLI/dev.ts
@@ -1,22 +1,31 @@
 import path from "path";
 import { configDotenv } from "dotenv";
-import { getAllFilesRecursive, loadModuleFromFile } from "..";
+import { ensureFramworkModule, getAllFilesRecursive, loadModuleFromFile, start } from "..";
 import { SOURCE_CODE_FILE_NAME_REGEX, SOURCE_CODE_FOLDER_NAME_REGEX } from "../constant";
-import { AnyCreateReturn } from "../types";
+import { A, AnyCreateReturn, C } from "../types";
 
 export default async function dev() {
+    // I'm not sure exactly what this is for anymore, but as far as I remember, without it, it doesn't work.
     configDotenv({
         "path": path.join(process.cwd(), ".env")
-    })
+    });
 
+    // Define the working directory
     const workDir = path.join(process.cwd(), "src");
 
+    // Get all source code files
     const filesPaths = getAllFilesRecursive(
         workDir,
         SOURCE_CODE_FILE_NAME_REGEX, // Match source code files
         SOURCE_CODE_FOLDER_NAME_REGEX // Match source code folders
-    )
+    );
+    // INFO: SOURCE_CODE_FILE_NAME_REGEX and SOURCE_CODE_FOLDER_NAME_REGEX are constant variable (a kind of global parameters)
 
-    const rawModules:{module:any, path:string}[] = filesPaths.map(filePath => ({ module: loadModuleFromFile(filePath), path: filePath }));
+    // Load the modules
+    const rawModules:A[] = filesPaths.map(filePath => ({ module: loadModuleFromFile(filePath), path: filePath }));
 
-    // const modules: {export: AnyCreateReturn, id: string} = 
+    start(
+        rawModules, // Pass the raw modules
+        true // Enable development mode (watcher, hot-reloading, etc.)
+    );
+}
