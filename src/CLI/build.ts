c1e4eec45b31dcb107df1de33822ed3836ce9b11
﻿ <98918089+Oignontom8283@users.noreply.github.com>
2025-09-12 15:51:34 +0200

    Implement build function using Vite and create accordJsVitPlugin for event handling

--- src/CLI/build.ts
+++ src/CLI/build.ts
@@ -6,41 +6,6 @@ export default async function build() {
     console.log("🚀 Building project...");
 
     
-    const srcDir = path.join(process.cwd(), "src");
-    if (!fs.existsSync(srcDir)) {
-        console.error("❌ Source directory does not exist. Please create a 'src' directory with an 'index.ts' file.");
-        return;
-    }
-
-    const entry = path.join(process.cwd(), "src", "index.ts");
-    if (fs.existsSync(entry)) {
-        console.error("❌ Entry file already exists. Please remove 'src/index.ts' before building.");
-        //return;
-    }
-
-    // Create entry file (index.ts)
-    fs.writeFileSync(entry, indexDefault.trim(), 'utf8');
-
-
-    const outputFolder = path.join(process.cwd(), "dist");
-    const outputFileName = 'bundle.js';
-
-    // if (fs.existsSync(outputFolder)) {
-    //     fs.rmSync(outputFolder, { recursive: true, force: true });
-    // }
-    // fs.mkdirSync(outputFolder, { recursive: true });
-
-
-    // try {
-    //     await compile(entry, outputFolder, outputFileName);
-    // } catch (err) {
-    //     console.error("❌ Compilation failed:", err);
-    //     return;
-    // }
-
-    // delete entry file after compilation
-    //fs.unlinkSync(entry);
-
 
     console.log("✅ Build completed successfully.");
 }
